4716ab3e5ff180bf8572c819eb4a6c76706a9518
khadidja cheref <118544193+lifeiswhatitis@users.noreply.github.com>
2024-03-28 13:20:47 +0000

    Add files via upload

---
+++ Code/game_data.py
@@ -0,0 +1,13 @@
+import pygame 
+level_0 = {
+    'terrain': '../NEA - Red Hood/levels/0/level_0_terrain.csv', 
+    'potions': '../NEA - Red Hood/levels/0/level_0_potions.csv', 
+    'willows': '../NEA - Red Hood/levels/0/level_0_willows.csv',
+    'boxes': '../NEA - Red Hood/levels/0/level_0_boxes.csv', 
+    'bushes': '../NEA - Red Hood/levels/0/level_0_bushes.csv', 
+    'ladders': '../NEA - Red Hood/levels/0/level_0_ladders.csv',
+    'grass': '../NEA - Red Hood/levels/0/level_0_grass.csv', 
+    'pointers': '../NEA - Red Hood/levels/0/level_0_pointers.csv', 
+    'enemies': '../NEA - Red Hood/levels/0/level_0_enemies.csv',
+    'player': '../NEA - Red Hood/levels/0/level_0_player.csv'}
+
---
+++ test code/settings.py
@@ -0,0 +1,17 @@
+level_map = [
+'                            ',  
+'                            ',  
+'                            ',  
+' XX    XXX                  ',  
+' XX P                       ',
+' XXXX        XX          XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1000
+screen_height = len(level_map) * tile_size 
+
